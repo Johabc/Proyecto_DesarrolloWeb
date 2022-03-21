ba2795eebc94f3de54d5ff16815ac69f2dca0cea
JuanDavidAR <aldana.juand@javeriana.edu.co>
2022-03-21 17:43:36 -0500

    Update anillos.component.ts

--- src/app/components/anillos/anillos.component.ts
+++ src/app/components/anillos/anillos.component.ts
@@ -14,55 +14,31 @@ export class AnillosComponent implements OnInit {
         nombre: 'Anillo luna y sol',
         precio: 1000,
         imagen:'imagen01.jpg',
-        descripcion:'descripcion',
+        descripcion:'luna y sol, ideal para parjeas',
         cantidad:100
     },
     {
         id:2,
         nombre:'Anillo always w/you',
-        precio:1000,
-        imagen :' ',
-        descripcion:'descripcion',
+        precio:2000,
+        imagen :'imagen02.jpg ',
+        descripcion:'Anillo que representa el amor ',
         cantidad:100
     },
     {
-        i:3,
+        id:3,
         nombre:'Anillos de boda',
-        precio:1000,
-        imagen :' ',
-        descripcion:'descripcion',
+        precio:2200,
+        imagen :'imagen03.jpg ',
+        descripcion:'Para el matrmonio',
         cantidad:100
     },
     {
-        i:4,
-        nombre:'Anillo luna y sol',
-        precio:1000,
-        imagen :' ',
-        descripcion:'descripcion',
-        cantidad:100
-    },
-    {
-        i:5,
-        nombre:'Anillo luna y sol',
-        precio:1000,
-        imagen :' ',
-        descripcion:'descripcion',
-        cantidad:100
-    },
-    {
-        i:6,
-        nombre:'Anillo luna y sol',
-        precio:1000,
-        imagen :' ',
-        descripcion:'descripcion',
-        cantidad:100
-    },
-    {
-        i:7,
-        nombre:'Anillo luna y sol',
-        precio:1000,
-        imagen :' ',
-        descripcion:'descripcion',
+        id:4,
+        nombre:'Anillo de amor',
+        precio:3200,
+        imagen :'imagen04.jpg ',
+        descripcion:'lleva el amor siempre contigo',
         cantidad:100
     }
 
@@ -71,6 +47,10 @@ export class AnillosComponent implements OnInit {
 
   ngOnInit(): void {
    
+  }
+
+  adicionarCarrito(id: any){
+    window.alert("Producto adicionado en el carrito");
   }
 
 
