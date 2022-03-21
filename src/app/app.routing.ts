37a2e1b9334d239e7e0b28bf9e0c9033df4c9330
Johanna Bolívar <71041573+Johabc@users.noreply.github.com>
2022-03-21 17:44:53 -0500

    guard

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,11 +2,12 @@ import { NgModule } from "@angular/core";
 import { Router, RouterModule } from "@angular/router";
 import { AnillosComponent } from "./components/anillos/anillos.component";
 import { CollaresComponent } from "./components/collares/collares.component";
-import { FormularioInicioSesionComponent } from "./components/formularios/formulario-inicio-sesion/formulario-inicio-sesion.component";
 import { InicioComponent } from "./components/inicio/inicio.component";
 import { PerfumesComponent } from "./components/perfumes/perfumes.component";
 import { PpRegistroComponent } from "./components/pp-registro/pp-registro.component";
 import { PulserasComponent } from "./components/pulseras/pulseras.component";
+import { NewUsersGuard } from "./new-users.guard";
+import { VerProductoComponent } from "./ver-producto/ver-producto.component";
 
 
 const routes = [
@@ -30,7 +31,11 @@ const routes = [
   },
   {
     path:'registro', component: PpRegistroComponent
+  },
+  {
+    path:'verProducto', component: VerProductoComponent, canActivate: [NewUsersGuard]
   }
+
 ]
 
 @NgModule(
