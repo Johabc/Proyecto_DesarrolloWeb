07165099df7835b77876bac671e4cd72021909e5
JuanDavidAR <aldana.juand@javeriana.edu.co>
2022-03-21 17:55:23 -0500

    Update collares.component.ts

--- src/app/components/collares/collares.component.ts
+++ src/app/components/collares/collares.component.ts
@@ -7,9 +7,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollaresComponent implements OnInit {
 
+
+  listaCollares=[{
+    id:1,
+    nombre: 'Collar luna y sol',
+    precio: 1000,
+    imagen:'imagen01.jpg',
+    descripcion:'luna y sol, ideal para parejas',
+    cantidad:100
+},
+{
+    id:2,
+    nombre:'Collar always w/you',
+    precio:2000,
+    imagen :'imagen02.jpg ',
+    descripcion:'Collar que representa el amor ',
+    cantidad:100
+},
+{
+    id:3,
+    nombre:'Collar de boda',
+    precio:2200,
+    imagen :'imagen03.jpg ',
+    descripcion:'Para el matrmonio',
+    cantidad:100
+},
+{
+    id:4,
+    nombre:'Collar de amor',
+    precio:3200,
+    imagen :'imagen04.jpg ',
+    descripcion:'lleva el amor siempre contigo',
+    cantidad:100
+}
+
+];
+
+
   constructor() { }
 
   ngOnInit(): void {
   }
+
+
+  adicionarCarrito(id: any){
+    window.alert("Producto adicionado en el carrito");
+  }
+
 
 }
