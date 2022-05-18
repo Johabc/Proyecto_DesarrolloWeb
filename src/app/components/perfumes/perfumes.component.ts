eec10c3b051d7d5f34fa1cb0c8ac2b3d6ea121ac
Johanna Bolívar <71041573+Johabc@users.noreply.github.com>
2022-05-18 13:26:31 -0500

    Productos

--- src/app/components/perfumes/perfumes.component.ts
+++ src/app/components/perfumes/perfumes.component.ts
@@ -7,9 +7,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfumesComponent implements OnInit {
 
+
+  listaPerfumes=[{
+    id:1,
+    nombre: 'Colonia prada',
+    precio: 850000,
+    imagen: 'imagen01.jpg',
+    descripcion: 'Colonia para hombre marca prada',
+    cantidad: 100
+  },
+  {
+    id:2,
+    nombre:'Colonia Sauvage by dior',
+    precio:900000,
+    imagen :'imagen02.jpg ',
+    descripcion:'Colonia para hombre marca dior',
+    cantidad:100
+  },
+  {
+    id:3,
+    nombre:'Perfume Versace',
+    precio:1200000,
+    imagen :'imagen03.jpg ',
+    descripcion:'Perfume para mujer marca Versace',
+    cantidad:100
+  },
+  {
+    id:4,
+    nombre:'Perfume Good Girl by Carolina Herrera',
+    precio:2500000,
+    imagen:'imagen04.jpg',
+    descripcion: 'Perfume para mujer marca Carolina Herrera',
+    cantidad: 100
+  }
+  ]
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  adicionarCarrito(id: any){
+    window.alert("Producto adicionado en el carrito");
+  }
 }
--- src/app/components/pulseras/pulseras.component.ts
+++ src/app/components/pulseras/pulseras.component.ts
@@ -7,9 +7,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PulserasComponent implements OnInit {
 
+  listaPulseras=[{
+    id:1,
+    nombre: 'Pulsera as it was',
+    precio: 85000,
+    imagen: 'imagen01.jpg',
+    descripcion: 'Pulsera de color negro con detalles plateados',
+    cantidad: 100
+  },
+  {
+    id:2,
+    nombre:'Pulsera polos opuestos',
+    precio:250000,
+    imagen :'imagen02.jpg ',
+    descripcion:'Pulseras de color negro y blanco para los polos opuestos',
+    cantidad:100
+  },
+  {
+    id:3,
+    nombre:'Pulsera bff',
+    precio:220000,
+    imagen :'imagen03.jpg ',
+    descripcion:'Pulseras con grabado en plateado y dorado que representa la amistad',
+    cantidad:100
+  },
+  {
+    id:4,
+    nombre:'Pulsera en oro',
+    precio:250000,
+    imagen:'imagen04.jpg',
+    descripcion: 'Pulsera en oro con perlas',
+    cantidad: 100
+  }
+  ]
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+
+  adicionarCarrito(id: any){
+    window.alert("Producto adicionado en el carrito");
   }
 
 }
